de91a0f3473c9260d4d86d37cfc50554decd5425
Yasin Kinyua Murithi <yasinkinyua@gmail.com>
2022-03-29 18:35:46 +0300

    Feature: Store data in database
    Signed-off-by: Yasin Kinyua Murithi <yasinkinyua@gmail.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 
@@ -13,7 +14,7 @@ func main() {
 	DB := db.DBConnection()
 
 	db.SeedDB(DB)
-	usingxmlquery()
+	usingxmlquery(DB)
 }
 
 type CurrencyTime struct {
@@ -25,23 +26,27 @@ type CurrencyValues struct {
 	CurrencyTime []CurrencyTime
 }
 
-func usingxmlquery() {
+func usingxmlquery(DB *sql.DB) {
 	doc, err := xmlquery.LoadURL("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml")
 	if err != nil {
 		log.Print(err)
 	}
 
 	lists, _ := xmlquery.QueryAll(doc, "//Cube//Cube")
+	var datetime string
 	for _, list := range lists {
+
 		if list.SelectAttr("time") == "" {
 			continue
 		}
 		fmt.Printf("Time: %s\n", list.SelectAttr("time"))
+		datetime = list.SelectAttr("time")
 		for _, value := range list.SelectElements("//Cube") {
 			if value.SelectAttr("currency") == "" || value.SelectAttr("currency") == "" {
 				continue
 			}
 			fmt.Printf("Currency: %s Rate %s\n", value.SelectAttr("currency"), value.SelectAttr("rate"))
+			db.Create(DB, value.SelectAttr("currency"), value.SelectAttr("rate"), datetime)
 		}
 	}
 }
